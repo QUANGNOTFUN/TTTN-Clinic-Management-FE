3f6dcb78840b158a6dbd4c04f44893ac4ed9da6f
HuynhNgocXuan <131683766+HuynhNgocXuan@users.noreply.github.com>
2025-09-19 16:18:47 +0700

    update(css): /doctor

--- src/app/(guest)/doctor/page.tsx
+++ src/app/(guest)/doctor/page.tsx
@@ -73,7 +73,7 @@ export default function DoctorPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-200 to-gray-50 dark:from-gray-800 dark:to-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="mb-8 ">
+        <div className="mb-8 pt-16">
           <h1 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-gray-200 text-center mb-8">
             Danh Sách Bác Sĩ
           </h1>
